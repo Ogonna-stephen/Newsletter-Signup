0d25db86d7d6563ccca9b32fd53f46a48907ca29
stephen <ogarakuogonna@gmail.com>
2024-02-09 17:07:20 +0100

    changed from https to fetch

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 const serverless = require("serverless-http");
 const router = express.Router();
 
@@ -15,7 +16,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
     let firstName = req.body.fName;
     let lastName = req.body.lName;
     let email = req.body.email;
@@ -36,24 +37,23 @@ app.post("/", (req, res) => {
     let jsonData = JSON.stringify(data);
 
     const url = "https://us21.api.mailchimp.com/3.0/lists/efd1177a3b"
-    const options = {
+    // const options = {
+    //     method: "POST",
+    //     auth: "Ogoo:8e0b5464c5b51471713e0529ddc626a5-us21"
+    // }
+
+    const response = await fetch(url, {
         method: "POST",
-        auth: "Ogoo:8e0b5464c5b51471713e0529ddc626a5-us21"
-    }
+        headers: {
+            Authorization: "Bearer 51f70bb3d9cff360a611a861c9f3ebc3-us21"
+        },
+        body: jsonData
+    })
+    
+    if(response.status === 200)
+        return res.sendFile(__dirname + "/success.html");
 
-    request = https.request(url, options, (response) => {
-        if(response.statusCode === 200){
-            res.sendFile(__dirname + "/success.html");
-        }else{
-            res.sendFile(__dirname + "/failure.html");
-        }
-
-        response.on("data", (data) => {
-            console.log(JSON.parse(data));
-        });
-    });
-    request.write(jsonData);
-    request.end();
+    res.sendFile(__dirname + "/failure.html");
 });
 
 app.post("/failure", (req, res) => {
@@ -68,7 +68,7 @@ app.use("/.netlify/functions/api", router);
 module.exports.handler = serverless(app);
 
 // API KEY
-// 8e0b5464c5b51471713e0529ddc626a5-us21
+// 51f70bb3d9cff360a611a861c9f3ebc3-us21
 
 // list ID
 // efd1177a3b
